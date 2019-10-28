a4bcfeb4665f8fc0d38c3923b1cf1ec8ab674fdb
risju62 <risju62@windowslive.com>
2019-10-28 16:15:01 +0200

    step5

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
 
-const persons =
+let persons =
 [
     {
         "name": "Arto Hellas",
@@ -29,9 +29,54 @@ const persons =
 ]
 
 
+const generateId = () =>{
+  return Math.floor((Math.random()*100) + 1)
+}
+
 app.get('/api/persons', (request, response) =>{
     console.log('Get persons')
     response.json(persons)
+})
+
+app.get('/api/persons/:id',(request, response) =>{
+  
+  const id = Number(request.params.id)
+  console.log ("get number for ", {id})
+  const person = persons.find(person =>{
+    return(person.id === id)
+  })
+  if (person){
+    response.json(person)
+  
+  }
+  else{
+    response.status(404).end()
+  }
+  
+})
+
+app.delete('/api/persons/:id', (request, response) =>{
+  const id = Number(request.params.id)
+  console.log("delete ",{id})
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
+})
+
+app.post('/api/persons',(request,response) =>{
+  const body=request.body
+  console.log(body)
+  if(!body.name){
+    return response.status(400).json({
+      error: 'name missing'
+    })
+  }
+  const person = {
+    "name": body.name,
+    "number": body.number,
+    "id":generateId()
+  }
+  persons = persons.concat(person)
+  response.json(person)
 })
 
 app.get('/info', (request, response) => {
